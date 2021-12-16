1667be07205584f8c525d1f9d37011ef4bbb3811
chafesolomon@gmail.com <chafesolomon@gmail.com>
2021-12-15 20:47:05 -0500

    Updated Code For Madlibs

---
+++ Madlibs.py
@@ -0,0 +1,8 @@
+q1 = input('Color ')
+q2 = input('Verb ')
+q3 = input('Planet ')
+q4 = input('Adjetive ')
+q5 = input('Adjetive ')
+
+phrase = f"{q1} foxes {q2} over the {q3} {q4} {q5}"
+print(phrase)
