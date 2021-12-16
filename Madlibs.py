04a6ae041b6ef71ec908b9440789ed313f11dd86
chafesolomon@gmail.com <chafesolomon@gmail.com>
2021-12-16 16:27:30 -0500

    reason

--- Madlibs.py
+++ Madlibs.py
@@ -4,5 +4,10 @@ q3 = input('Planet ')
 q4 = input('Adjetive ')
 q5 = input('Adjetive ')
 
-phrase = f"{q1} foxes {q2} over the {q3} {q4} {q5}"
-print(phrase)
+not_allowed = ['1','2','3','4','5','6','7','8','9','0']
+if not_allowed in q1 or q2 or q3 or q4 or q5:
+    phrase = f"{q1} foxes {q2} over the {q3} {q4} {q5}"
+    print(phrase)
+
+else:
+    print('Variable uses intger')
