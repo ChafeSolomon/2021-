45cf14a91a02a539ed16f7f0bee3bc5250ad6b04
chafesolomon@gmail.com <chafesolomon@gmail.com>
2021-12-17 23:27:14 -0500

    Madlibs

--- Madlibs.py
+++ Madlibs.py
@@ -1,7 +1,13 @@
+q1 = input('Color ')
+q2 = input('Verb ')
+q3 = input('Planet ')
+q4 = input('Adjetive ')
+q5 = input('Adjetive ')
 
+not_allowed = ['1','2','3','4','5','6','7','8','9','0']
+if not_allowed in q1 or q2 or q3 or q4 or q5:
+    phrase = f"{q1} foxes {q2} over the {q3} {q4} {q5}"
+    print(phrase)
 
-madlibs_ready = False
-def var_setter():
-    while madlibs_ready != True:
-        for i in var:
-            input("Give an adjetive")
+else:
+    print('Variable uses intger')
